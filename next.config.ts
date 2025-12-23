99790feef0d2c9c686b4f0a2daa9889c21e0d189
gledsonsantos <gledsonvicente@gmail.com>
2025-12-23 09:53:50 -0300

    chore: atualiza .gitignore e removendo basePath and assetPrefix from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const repo = 'portfolio'; // <-- troque pelo nome do seu repo
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: 'export',
   trailingSlash: true,
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
 };
 
 export default nextConfig;
